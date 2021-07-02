14fc76a0772cb1748f8381062bb450f0cf969e38
Hibiki183 <s1280183@u-aizu.ac.jp>
2021-07-02 14:47:55 +0900

    Initial revision

---
+++ DiceGame.py
@@ -0,0 +1,17 @@
+import random
+def RollDice():
+    dice = random.randint(1, 6)
+    return dice
+
+dice = []
+
+dice.append(RollDice())
+dice.append(RollDice())
+
+print('Rolling the dice...')
+for i in range(2):
+    print(f'Dice {i + 1}: {dice[i]}')
+
+totalValue = dice[0] + dice[1]
+
+print(f'Total Value: {totalValue}')
